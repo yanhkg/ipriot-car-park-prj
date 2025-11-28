0129c4b4d009bb9b270086d95f8c5e909f493df5
Toby Yan <20156693@tafe.wa.edu.au>
2025-11-28 08:52:25 +0800

    chore:added the log car activity feature

--- src/car_park.py
+++ src/car_park.py
@@ -1,13 +1,18 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime
 
 class CarPark:
-    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        # create the file if it doesn't exist:
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car Park at {self.location}, with {self.capacity} bays."
@@ -24,10 +29,12 @@ class CarPark:
     def add_car(self, plate):
         self.plates.append(plate)
         self.update_displays()
+        self._log_car_activity(plate, "entered")
 
     def remove_car(self, plate):
         self.plates.remove(plate)
         self.update_displays()
+        self._log_car_activity(plate, "exited")
 
     @property
     def available_bays(self):
@@ -40,3 +47,7 @@ class CarPark:
         data = {"available_bays": self.available_bays, "temperature": 25}
         for display in self.displays:
             display.update(self.available_bays)
+
+    def _log_car_activity(self, plate, action):
+        with self.log_file.open("a") as f:
+            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
--- tests/test_car_park.py
+++ tests/test_car_park.py
@@ -70,9 +70,9 @@ class TestCarPark(unittest.TestCase):
           self.car_park.remove_car("NEW-001")
           with self.car_park.log_file.open() as f:
               last_line = f.readlines()[-1]
-          self.assertIn(last_line, "NEW-001")  # check plate entered
-          self.assertIn(last_line, "exited")  # check description
-          self.assertIn(last_line, "\n")  # check entry has a new line
+          self.assertIn("NEW-001", last_line)  # check plate entered
+          self.assertIn("exited", last_line)  # check description
+          self.assertIn("\n", last_line)  # check entry has a new line
 
 if __name__ == "__main__":
    unittest.main()
