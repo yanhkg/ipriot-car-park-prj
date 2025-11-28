e8babacc2a1230ebc22d0232828f43b509e8157b
Toby Yan <20156693@tafe.wa.edu.au>
2025-11-28 08:57:44 +0800

    chore:added config functionality

--- src/car_park.py
+++ src/car_park.py
@@ -2,9 +2,10 @@ from sensor import Sensor
 from display import Display
 from pathlib import Path
 from datetime import datetime
+import json
 
 class CarPark:
-    def __init__(self, location, capacity, plates=None, sensors=None, displays=None, log_file=Path("log.txt")):
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None, log_file=Path("log.txt"), config_file=Path("config.json")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
@@ -13,6 +14,9 @@ class CarPark:
         self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
         # create the file if it doesn't exist:
         self.log_file.touch(exist_ok=True)
+        self.config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        # create the file if it doesn't exist:
+        self.config_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car Park at {self.location}, with {self.capacity} bays."
@@ -50,4 +54,17 @@ class CarPark:
 
     def _log_car_activity(self, plate, action):
         with self.log_file.open("a") as f:
-            f.write(f"{plate} {action} at {datetime.now():%Y-%m-%d %H:%M:%S}\n")
+            f.write(f"{plate} {action} at {datetime.now()}\n")
+
+    def write_config(self):
+       with open(self.config_file, "w") as f:
+          json.dump({"location": self.location,
+                     "capacity": self.capacity,
+                     "log_file": str(self.log_file)}, f)
+
+    @classmethod
+    def from_config(cls, config_file=Path("config.json")):
+        config_file = config_file if isinstance(config_file, Path) else Path(config_file)
+        with config_file.open() as f:
+            config = json.load(f)
+        return cls(config["location"], config["capacity"], log_file=config["log_file"])
